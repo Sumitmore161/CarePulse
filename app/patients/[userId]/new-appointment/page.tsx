ade331e51e8eb4789081c905d2e6c8eb2cb07793
Sumitmore161 <sumitmore161@gmail.com>
2025-09-16 14:46:15 +0530

    done few changes in new-appointement/page.tsx

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -22,6 +22,7 @@ const Appointment = async ({ params: { userId } }: SearchParamProps) => {
             patientId={patient?.$id}
             userId={userId}
             type="create"
+            timeZone={new Date().toString()}
           />
 
           <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
